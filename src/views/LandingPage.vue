d2254291a13b26e66642ee1dc94229b461a09bc3
Marco Ring <marco.ring00@gmail.com>
2021-01-05 14:54:39 +0100

    added team name and color input component

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,29 +1,67 @@
 <template>
   <div>
-    <v-flex class="pa-6 pb-3 text-center">
+    <v-flex class="text-center">
       <v-img
         :src="require('@/assets/logo.png')"
-        style="width: 30%; height: 30%"
-        class="mx-auto justify-center"
+        style="width: 25%; height: 25%"
+        class="mx-auto align-center justify-center"
       />
     </v-flex>
+<div class="mt-6">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+     <v-col
+            cols="12"
+            sm="6"
+            md="4"
+          >
+            <v-text-field
+              label="Enter TEAM NAME"
+              solo
+              dense
+            ></v-text-field>
+          </v-col>
+           </v-row>
+    </v-container>
+</div>
 
-    <div id="app">
-      <v-app id="inspire">
+    
+<div>
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col
+          cols="12"
+            sm="6"
+            md="4"
+        >
+          <v-select
+            :items="items"
+            label="Choose TEAM COLOR ..."
+            solo
+            dense
+          ></v-select>
+        </v-col>
+      </v-row>
+    </v-container>
+</div>
+
+    <div>
         <div class="text-center">
           <v-btn v-btn-style rounded color="#4E9455" dark link to="dashboard">
             Go to Dashboard
           </v-btn>
         </div>
-      </v-app>
     </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  data() {},
-};
+    data: () => ({ 
+    items: ['Red', 'Blue', 'Green', 'Orange'],
+    })
+}
 </script>
 
 <style>
