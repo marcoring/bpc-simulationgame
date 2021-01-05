8c31cd7eb149342c27d3fed4d8c9669a79f1c63f
Maksym Bondarenko <max.druppy@gmail.com>
2021-01-05 11:47:02 +0200

    dashboard base-elements created

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,77 @@
 <template>
-<div>
-    <h1>
-        Hallo Welt
-    </h1>
-</div>
+  <div>
+    <v-row class="pa-3 text-left">
+      <v-col>
+        <h1>Dashboard</h1>
+        <h3>{{ teamName }}</h3>
+      </v-col>
+      <v-img
+        :src="require('@/assets/logo.png')"
+        style="width: 50px; height: 50px; text-align: left"
+        class="mx-auto justify-left"
+      />
+      <v-spacer />
+      <v-btn rounded color="#4E9455" dark @click="endRound"> End Round </v-btn>
+    </v-row>
+
+    <v-row class="pa-6 text-left">
+      <h2>Round {{ round }}</h2>
+      <v-progress-linear
+        color="light-green darken-4"
+        height="20"
+        v-model="calculateProgress"
+        rounded
+        striped
+      >
+        <strong>{{ calculateProgress }}%</strong>
+      </v-progress-linear>
+    </v-row>
+
+    <v-row class="pa-10 text-center">
+      <v-col v-for="element in progressElements" :key="element.id">
+        <v-progress-circular
+          :id="element.id"
+          :width="15"
+          :rotate="-90"
+          :size="100"
+          :value="element.value"
+          color="teal"
+        >
+          {{ element.value }}
+        </v-progress-circular>
+        <h3>{{ element.name }}</h3>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      teamName: "Team Name",
+      round: 1,
+      progressElements: [
+        { id: "purchaising", name: "Purchaising", value: 15 },
+        { id: "delivery", name: "Delivery", value: 45 },
+      ],
+    };
+  },
+  methods: {
+    endRound() {
+      console.log("End Round");
+    },
+  },
+  computed: {
+    calculateProgress() {
+      var sum = 0;
+      for (var i = 0; i < this.progressElements.length; i++) {
+        sum += this.progressElements[i].value;
+      }
+      return Math.ceil(sum / this.progressElements.length);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,43 +1,33 @@
 <template>
-<div>
+  <div>
     <v-flex class="pa-6 pb-3 text-center">
-    <v-img 
+      <v-img
         :src="require('@/assets/logo.png')"
-        style="width:30%; height:30%"
+        style="width: 30%; height: 30%"
         class="mx-auto justify-center"
-        />
-        </v-flex>
+      />
+    </v-flex>
 
-
-<div id="app">
-  <v-app id="inspire">
-    <div class="text-center">
-      <v-btn v-btn-style
-        rounded
-        color="#4E9455"
-        dark>
-        Go to Dashboard
-      </v-btn>
+    <div id="app">
+      <v-app id="inspire">
+        <div class="text-center">
+          <v-btn v-btn-style rounded color="#4E9455" dark link to="dashboard">
+            Go to Dashboard
+          </v-btn>
+        </div>
+      </v-app>
     </div>
-  </v-app>
-  
-</div>
-
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-
-    }
-
-}
+  data() {},
+};
 </script>
 
 <style>
 #inspire img {
-    width:100%,
+  width: 100%;
 }
-
 </style>
