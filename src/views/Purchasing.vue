86f2b9fb81a86446744d105d3623eab1cd01f2f8
Maksym Bondarenko <max.druppy@gmail.com>
2021-01-21 23:27:25 +0100

    Update Purchasing.vue
    Purchaising - added functionality, components

--- src/views/Purchasing.vue
+++ src/views/Purchasing.vue
@@ -51,16 +51,59 @@
       <!-- Managing purchaising process -->
       <v-row style="margin-top: 10px;">
         <h2 style="text-align: left;">Manage purchaising process</h2>
+      </v-row>
+      <v-row>
         <v-col>
-          <!-- -->
-          <v-card></v-card>
+          <v-select :items="vendors" label="Choose vendor..." />
+          <v-slider
+            v-model="quality.val"
+            :label="quality.label"
+            :thumb-color="primary"
+            thumb-label="always"
+          />
         </v-col>
 
         <v-col>
-          <!-- -->
-          <v-card></v-card>
+          <v-data-table
+            :headers="headersPurchaising"
+            :items="dataPurchaising"
+          />
         </v-col>
       </v-row>
+
+      <v-dialog v-model="confirmChangesDialog" persistant width="30%">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" rounded dark v-bind="attrs" v-on="on">
+            <v-icon left>
+              mdi-check-outline
+            </v-icon>
+            Accept changes
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-text>
+            Are you sure you want to confirm changes?
+          </v-card-text>
+          <v-divider />
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="primary"
+              text
+              @click="
+                confirmChangesDialog = false;
+                confirmChanges();
+              "
+            >
+              Accept
+            </v-btn>
+            <v-btn color="red" text @click="confirmChangesDialog = false">
+              Declaine
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
   </v-app>
 </template>
@@ -70,6 +113,29 @@ export default {
   name: "Purchaising",
   data() {
     return {
+      vendors: ["Vendor 1", "Vendor 2", "Vendor 3"],
+      quality: { label: "Quality (%)", val: 50, color: "primary" },
+      headersPurchaising: [
+        { text: "Material", value: "material" },
+        { text: "Cost per material (EUR)", value: "costPerMat" },
+        { text: "Amount (PC)", value: "amount" },
+        { text: "Total cost (EUR)", value: "totalCost" },
+      ],
+      dataPurchaising: [
+        {
+          material: "material 1",
+          costPerMat: "500",
+          amount: "55",
+          totalCost: "27.500",
+        },
+        {
+          material: "material 2",
+          costPerMat: "100",
+          amount: "80",
+          totalCost: "8.000",
+        },
+      ],
+      confirmChangesDialog: false,
       searchPrevRound: "",
       searchCurRound: "",
       headersRound: [
@@ -117,6 +183,12 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    confirmChanges() {
+      console.log("Purchaising-changes COMPLETED");
+      // Todo: send data (as oData) to Backend
+    },
   },
   props: {
     progressElements: Array,
